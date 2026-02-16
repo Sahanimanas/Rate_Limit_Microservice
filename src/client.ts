f2de09899708ce4d0dcfa1747acb29fde2452377
Sahanimanas <manashvisahani@gmail.com>
2026-02-16 12:12:29 +0530

    grpc server created

---
+++ src/client.ts
@@ -0,0 +1,39 @@
+import grpc from '@grpc/grpc-js';
+import grpcloader from '@grpc/proto-loader';
+import path from 'path';
+
+const PROTO_PATH = path.join(import.meta.dirname, 'ratelimit.proto');
+
+const protodefinition = grpcloader.loadSync(PROTO_PATH);
+
+const proto = grpc.loadPackageDefinition(protodefinition).ratelimiter;
+
+const client = new (proto as any).ratelimit('localhost:50051', grpc.credentials.createInsecure());
+
+async function testRatelimit(userId:number = 1) {
+ 
+
+  for (let i = 0; i < 15; i++) {
+    client.checklimit({ userid: userId }, (err: any, response: any) => {
+      if (err) {
+        console.error('Error:', err);
+      } else {
+        console.log(`Request: User ${userId} - Count: ${response.count}, Allowed: ${response.allowed}`);
+      }
+    });
+}
+
+  // Give time for all requests to complete
+  setTimeout(() => {
+    process.exit(0);
+  }, 1000);
+}
+
+
+testRatelimit(1);
+// testRatelimit(2);
+// testRatelimit(3);
+// testRatelimit(4);
+// testRatelimit(5);
+// testRatelimit(6);
+// testRatelimit(7);
---
+++ src/grpc_server.ts
@@ -0,0 +1,70 @@
+import grpc from '@grpc/grpc-js';
+import grpcloader from '@grpc/proto-loader';
+import {Redis} from 'ioredis';
+import path from 'path';
+// import Redis from 'ioredis';
+
+const PROTO_PATH  = path.join(import.meta.dirname, 'ratelimit.proto');
+
+const protodefinition = grpcloader.loadSync(PROTO_PATH);
+
+const proto = grpc.loadPackageDefinition(protodefinition).ratelimiter;
+
+const redis = new Redis({
+    port: 6001,
+    host: 'localhost'
+});
+redis.on('connect', () => {
+    console.log('Connected to Redis');
+});
+redis.on('error', (err) => {
+    console.error('Redis connection error:', err);
+});
+
+
+const MAX_REQUESTS = 10;
+const WINDOW_SIZE = 60; // seconds
+
+async function checklimit(call: any, callback: any) {
+  const userId = call.request.userid;
+  const key = `ratelimit:${userId}`;
+
+  const luaScript = `
+  local current = redis.call("INCR", KEYS[1])
+  if current == 1 then
+    redis.call("EXPIRE", KEYS[1], ARGV[1])
+  end
+  return current
+  `;
+
+  try {
+    const current = await redis.eval(luaScript, 1, key, WINDOW_SIZE) as number;
+
+    if (current <= MAX_REQUESTS) {
+      callback(null, { count: current, allowed: 'true' });
+    } else {
+      callback(null, { count: current, allowed: 'false' });
+    }
+  } catch (error) {
+    callback(error);
+  }
+}
+
+function main(){
+    const server = new grpc.Server();
+    server.addService((proto as any).ratelimit.service, { Checklimit:checklimit });
+    
+    const PORT = 50051;
+    server.bindAsync(`0.0.0.0:${PORT}`, grpc.ServerCredentials.createInsecure(), (err: any, port: any) => {
+      if (err) {
+        console.error('Failed to bind server:', err);
+        return;
+      }
+     else
+      console.log(`gRPC Server running on port ${PORT}`);
+    });
+}
+
+main();
+
+
